9e5956dd41efd56d88363df17269e06657813846
adamw415 <121736352+adamw415@users.noreply.github.com>
2023-01-02 21:13:36 -0500

    Update Spotify_API_Extraction.py

--- Spotify_API_Extraction.py
+++ Spotify_API_Extraction.py
@@ -1,14 +1,15 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
+#import sqlite3
 from pathlib import Path
 import numpy as np
 from datetime import date
 from datetime import timedelta
 
 #Authentication
-cid = '____'
-secret = '____'
+cid = '515baa9f59c6413895637a859d3c3d32'
+secret = '5b6b4310c2754fd4b35817444c4913c9'
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
@@ -23,10 +24,12 @@ release_date = []
 duration = []
 release_date_precision = []
 
+cur_year = date.today().year
+search_year = '\'year:' + str(cur_year) +'\''
 
 #extract data from Spotify API
 for i in range(0,1000,50):
-    track_results = sp.search(q='year:2023', type='track', limit=50,offset=i)
+    track_results = sp.search(q=search_year, type='track', limit=50,offset=i)
     for i, j in enumerate(track_results['tracks']['items']):
         artist_name.append(j['artists'][0]['name'])
         track_name.append(j['name'])
@@ -53,12 +56,14 @@ df_tracks['duration_seconds'] = df_tracks['duration']/1000
 df_tracks['duration_seconds'] = [round(x) for x in df_tracks['duration_seconds']]
 df_tracks.drop('duration', axis = 1, inplace = True)
 
-#dudup
-df_tracks.drop_duplicates(inplace=True)
-df_popularity.drop_duplicates(inplace=True)
 
 #filter for release dates from yesterday to append to existing table
 df_tracks = df_tracks[df_tracks['release_date'] == str(date.today() - timedelta(days = 1))]
+
+
+#dudup
+df_tracks.drop_duplicates(inplace=True)
+df_popularity.drop_duplicates(inplace=True)
 
 #add bucket and indicator columns for analysis
 df_tracks['duration_category'] = np.select([(df_tracks.duration_seconds < 150),(df_tracks.duration_seconds >= 150) & (df_tracks.duration_seconds <= 240), \
